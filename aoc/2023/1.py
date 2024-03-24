40995e0c82126f54365031bce1fcee501d5f07b0
Dmitry Zhukov <zhukovdm@proton.me>
2024-03-24 16:09:17 +0100

    add several aoc solutions

--- aoc/2023/1.py
+++ aoc/2023/1.py
@@ -1,12 +1,26 @@
-import sys
+import sys, re
 from typing import List
 
-def main(args:List[str]):
+def main(args: List[str]) -> None:
     sum = 0
+    dic = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+    reg = r"(?=(1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine))"
+
     with open(args[1]) as file:
         for line in file:
-            digits = list(filter(lambda x: x.isdigit(), line))
-            sum += int(digits[0]) * 10 + int(digits[-1])
+            lst = [dic.get(word, word) for word in re.findall(reg, line)]
+            sum += 10 * int(lst[0]) + int(lst[-1])
+
     print(sum)
 
 main(sys.argv)
---
+++ aoc/2023/2.py
@@ -0,0 +1,26 @@
+import sys
+from typing import List
+
+def main(args: List[str]) -> None:
+    with open(args[1]) as file:
+        result = 0
+        for line in file:
+            r = 0
+            g = 0
+            b = 0
+            for sep in [":", ";", ","]:
+                line = line.replace(sep, " ")
+            line = list(filter(lambda word: word != "", line.strip().split(" ")))
+            for i in range(2, len(line), 2):
+                inc = int(line[i])
+                match line[i + 1]:
+                    case "red":
+                        r = max(r, inc)
+                    case "green":
+                        g = max(g, inc)
+                    case "blue":
+                        b = max(b, inc)
+            result += r * g * b
+        print(result)
+
+main(sys.argv)
