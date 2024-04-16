c5050b741fbc0ddad2a3e52d340d9801c046487c
Dmitry Zhukov <zhukovdm@proton.me>
2024-04-16 21:47:21 +0200

    concise sparse decompose

--- codility/solutions/sparse-binary-decomposition.cpp
+++ codility/solutions/sparse-binary-decomposition.cpp
@@ -3,11 +3,6 @@ bool is_set(int n, int i)
     return (n >> i) & 1;
 }
 
-int set(int n, int i)
-{
-    return n | (1 << i);
-}
-
 int unset(int n, int i)
 {
     return -1 & ~(1 << i) & n;
@@ -15,14 +10,10 @@ int unset(int n, int i)
 
 int solution(int n)
 {
-    int r = 0;
-
     for (int i = 1; i < 31; ++i) {
         if (is_set(n, i) && is_set(n, i - 1)) {
-            r = set(r, i);
             n = unset(n, i);
         }
     }
-
-    return r;
+    return n;
 }
