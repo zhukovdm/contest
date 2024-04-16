35308f72e6da7235f10d878c9aa6b5227feb76b2
Dmitry Zhukov <zhukovdm@proton.me>
2024-04-16 17:54:52 +0200

    add codility bitmasks

---
+++ codility/count-conforming-bitmasks.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+int count(int n)
+{
+    int cnt = 0;
+    for (int i = 0; i < 30; ++i) {
+        cnt = ((n >> i) & 1) ? cnt + 1 : cnt;
+    }
+    return 1 << (30 - cnt);
+}
+
+int solution(int A, int B, int C)
+{
+    vector<int> v{A, B, C, A | B, A | C, B | C, A | B | C};
+    for (auto &&item : v)
+        item = count(item);
+    return v[0] + v[1] + v[2] - v[3] - v[4] - v[5] + v[6];
+}
