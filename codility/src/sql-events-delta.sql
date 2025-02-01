18e7f07d518d46c201e594cb68cfb231264389b1
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2025-02-01 20:10:52 +0100

    refactoring for codility and aoc problems

--- codility/src/sql-events-delta.sql
+++ codility/src/sql-events-delta.sql
@@ -6,13 +6,13 @@ SELECT FST.event_type AS event_type, (FST.value - SND.value) AS value FROM
 (
     SELECT * FROM events AS A
         WHERE NOT EXISTS (
-            SELECT * FROM events AS B WHERE A.event_type = B.event_type AND A.time < B.time)
+            SELECT 1 FROM events AS B WHERE A.event_type = B.event_type AND A.time < B.time)
 ) AS FST
 INNER JOIN
 (
     SELECT * FROM events AS A
         WHERE 1 = (
-            SELECT COUNT(*) FROM events AS B WHERE A.event_type = B.event_type AND A.time < B.time)
+            SELECT COUNT(1) FROM events AS B WHERE A.event_type = B.event_type AND A.time < B.time)
 ) AS SND
 ON FST.event_type = SND.event_type
 ORDER BY FST.event_type
