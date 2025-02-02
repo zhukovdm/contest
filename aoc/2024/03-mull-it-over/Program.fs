99817de64a6b565a7d0359aef1fe6170815f501e
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2025-02-02 23:34:09 +0100

    add aoc 2024 case 3 part 1

---
+++ aoc/2024/03-mull-it-over/Program.fs
@@ -0,0 +1,25 @@
+﻿(*
+    https://adventofcode.com/2024/day/3
+
+    Answer: 190604937
+*)
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+let readFile path =
+    (new StreamReader(File.OpenRead(path))).ReadToEnd()
+
+[<EntryPoint>]
+let main argv =
+    let input = readFile argv[0]
+    let regex = Regex(@"mul\((\d{1,3}),(\d{1,3})\)")
+
+    let result =
+        regex.Matches(input)
+        |> Seq.map (fun m -> (Int32.Parse m.Groups[1].Value) * (Int32.Parse m.Groups[2].Value))
+        |> Seq.fold (+) 0
+
+    printfn "%d" result
+    0
