21540afc7bbfc268caa7fecfc1dd19ec26f94ef1
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2025-02-02 22:54:48 +0100

    add aoc 2024 part 2

--- aoc/2024/02-red-nosed-reports/Program.fs
+++ aoc/2024/02-red-nosed-reports/Program.fs
@@ -1,7 +1,8 @@
 ﻿(*
     https://adventofcode.com/2024/day/2
 
-    Answer: 
+    Answer, Part 1: 486
+    Answer, Part 2: 540
 *)
 
 open System
@@ -14,6 +15,18 @@ let readLines path =
         while not reader.EndOfStream do
             reader.ReadLine()
     }
+
+let selfZip (xs: seq<int>) = Seq.zip xs (Seq.skip 1 xs)
+
+let withRemovedItem (xs: seq<int>) =
+    seq { 0 .. (Seq.length xs) - 1 }
+    |> Seq.map (fun ir ->
+        Seq.mapi (fun ix x -> if ix = ir then None else Some x) xs
+        |> Seq.choose (fun x -> x))
+    |> Seq.map selfZip
+
+let expandLine (xs: seq<int>) =
+    Seq.concat [ (xs |> selfZip |> Seq.singleton); (withRemovedItem xs) ]
 
 let allIncreasing (xs: seq<(int * int)>) =
     xs
@@ -31,8 +44,10 @@ let main argv =
         data
         |> Seq.map (fun line -> Array.toSeq (line.Split(' ', StringSplitOptions.RemoveEmptyEntries)))
         |> Seq.map (fun line -> Seq.map Int32.Parse line)
-        |> Seq.map (fun line -> Seq.zip line (Seq.skip 1 line))
-        |> Seq.map (fun line -> allIncreasing line || allDecreasing line)
+        |> Seq.map (fun line -> expandLine line)
+        |> Seq.map (fun arrs ->
+            arrs
+            |> Seq.fold (fun acc xs -> acc || allIncreasing xs || allDecreasing xs) false)
         |> Seq.fold (fun acc all -> if all then acc + 1 else acc) 0
 
     printfn "%d" result
